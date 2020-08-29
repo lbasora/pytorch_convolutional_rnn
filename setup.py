73413957f6cf2a6f7f0ffcc62b4fb3025a24df9a
lbasora <luis.basora@onera.fr>
2020-08-29 17:13:04 +0200

    setup corrected

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="pytorch_convolutional_rnn",
+    name="convolutional_rnn",
     description="Pytorch Convolutional RNN",
     packages=find_packages(),
     install_requires=["torch", "typing_extensions"],
